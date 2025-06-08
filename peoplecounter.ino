c74187c320cb029f05f4c14c885aded30fbf46cc
Ensase <ensase@mail.ru>
2025-06-08 15:57:11 +0300

    Added SD card and RTC support

--- peoplecounter.ino
+++ peoplecounter.ino
@@ -1,3 +1,8 @@
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
+#include <SPI.h>
+#include <SD.h>
 #include <NewPing.h>
 
 #define SONAR_NUM 2      // Number of sensors.
@@ -11,8 +16,8 @@
 #define BACKWARD 1
 
 NewPing sonar[SONAR_NUM] = {   // Sensor object array.
-  NewPing(3, 5, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
-  NewPing(4, 6, MAX_DISTANCE)
+  NewPing(9, 8, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(7, 6, MAX_DISTANCE)
 };
 int state = WAIT_ANY;
 int dir = FORWARD;
@@ -22,10 +27,25 @@ int countend = 0;
 int countwait = 0;
 int skipwait = 7;
 int waitend = 0;
+
+File myFile;
+
 void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   unsigned long a1, a2;
   long d1, d2, dd, sum;
+  Serial.begin(115200);
+  while (!Serial) ; // wait for serial
+  delay(200);
+  Serial.println("People Counter Test");
+  Serial.println("-------------------");
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
   sum = 0;
   Serial.println("Calibration:");
   for (uint8_t j = 0; j < 20; j++) {
@@ -44,11 +64,11 @@ void setup() {
   offset = sum / 20;
   Serial.println(offset);
 
-
 }
-int personin = 0;
-int personout = 0;
-void loop() {
+
+int detectFSM() {
+  int personin = 0;
+  int personout = 0;
   unsigned long a1, a2;
   long d1, d2, dd;
   
@@ -135,5 +155,73 @@ void loop() {
   Serial.print(" ");
   Serial.print(state);
   Serial.println();
+  if (personin > 0) {
+    return personin;
+  } else if (personout > 0) {
+    return -personout;
+  }
+  return 0;
 }
+
+void loop() {
+  tmElements_t tm;
+  int person = 0;
+
+  while (true) {
+    person = detectFSM();
+    if (person != 0) break;
+  }
+
+  if (RTC.read(tm)) {
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    char fileName[16];
+    char timeText[16];
+    char dateText[16];
+    sprintf(fileName, "%04d%02d%02d.txt", tmYearToCalendar(tm.Year), tm.Month, tm.Day);
+    sprintf(dateText, "%04d-%02d-%02d", tmYearToCalendar(tm.Year), tm.Month, tm.Day);
+    sprintf(timeText, "%02d:%02d:%02d", tm.Hour, tm.Minute, tm.Second);
+    Serial.print(dateText);
+    Serial.print(" ");
+    Serial.print(timeText);
+    Serial.print(" ");
+    Serial.println(person);
+
+    myFile = SD.open(fileName, FILE_WRITE);
+    // if the file opened okay, write to it:
+    if (!myFile) {
+      // if the file didn't open, print an error:
+      Serial.print("error opening ");
+      Serial.println(fileName);
+    } else {
+      Serial.print("Writing to ");
+      Serial.print(fileName);
+
+
+
+      myFile.print(dateText);
+      myFile.print(" ");
+      myFile.print(timeText);
+      myFile.print(" ");
+      myFile.println(person);
+
+      // close the file:
+      myFile.close();
+      Serial.println(" - done.");
+    }
+
+  } else {
+    if (RTC.chipPresent()) {
+      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+      Serial.println("example to initialize the time and begin running.");
+      Serial.println();
+    } else {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+      Serial.println();
+    }
+    delay(9000);
+  }
+  //delay(1000);
+}
+
 
